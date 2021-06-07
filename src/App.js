8f04a164de9d3ac0b17d55b07f5924f1cb04acc5
Mohammad Moien <mnmoienian@gmail.com>
2021-06-07 08:57:55 +0430

    create signIn page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import * as ROUTES from "./constants/routes";
+
+const Login = lazy(() => import("./pages/Login"));
+
 function App() {
-  return <p>Hello from Instagram</p>;
+  return (
+    <Router>
+      <Suspense fallback="Loading">
+        <Switch>
+          <Route path={ROUTES.LOGIN} component={Login} />
+        </Switch>
+      </Suspense>
+    </Router>
+  );
 }
 
 export default App;
